3628763fb20da4732e426ee94af44c6072be578b
Rudy N. Caña <rudy_cana@cody.inc>
2026-01-23 18:36:12 +0800

    save

--- client/electron/src/main.ts
+++ client/electron/src/main.ts
@@ -1,31 +1,70 @@
-import { app, BrowserWindow } from "electron";
+import { app, BrowserWindow, Tray, Menu, nativeImage } from "electron";
 import path from "path";
 
+let mainWindow: BrowserWindow | null;
+let tray: Tray | null;
+let isQuiting = false;
+
 const createWindow = () => {
-  const win = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     title: "Centralize PESO NET",
+    frame: false,
+    fullscreen: true,
+    // autoHideMenuBar: true,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      preload: path.join(__dirname, 'preload.js'),
+      devTools: false,
+      nodeIntegration: false,
+      contextIsolation: true    
     }
   })
 
-  win.loadFile(path.join(__dirname, "index.html"));
+  mainWindow.loadFile(path.join(__dirname, "index.html"));
+
+  mainWindow.on("close", (e) => {
+    if (!isQuiting) {
+      e.preventDefault();
+      mainWindow?.hide();
+    }
+  });
 }
 
-app.whenReady().then(() => {
-  createWindow()
-
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
+// Create tray
+const createTray = () => {
+  // Use an empty NativeImage when no icon is desired to satisfy the Tray constructor types.
+  tray = new Tray(nativeImage.createEmpty());
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: "Show App",
+      click: () => mainWindow?.show()
+    },
+    {
+      label: "Exit",
+      click: () => {
+        isQuiting = true;
+        app.quit();
+      }
     }
-  })
-})
+  ]);
+  tray.setToolTip("Centralize PESO NET");
+  tray.setContextMenu(contextMenu);
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+  tray.on("double-click", () => mainWindow?.show());
+};
+
+// App ready
+app.whenReady().then(() => {
+  createWindow();
+  createTray();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+// Prevent quitting on window close
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
